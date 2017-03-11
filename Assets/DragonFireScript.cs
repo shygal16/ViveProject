2da81bc065e408a48358ea052fad7844674d88f9
shyguy16 <eashy16@gmail.com>
2017-03-10 17:26:09 -0800

    Dragon Controls
    Using first person controler
    need to make it continually move

---
+++ Assets/DragonFireScript.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DragonFireScript : MonoBehaviour {
+
+    public Rigidbody rb;
+
+    public GameObject Projectile;
+    public GameObject TargetTip;
+    public float ProjectileSpeed = 10;
+
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+        if (Input.GetKey(KeyCode.Space))
+        {
+            GameObject p = Instantiate(Projectile, TargetTip.transform.position, TargetTip.transform.rotation) as GameObject;
+            Rigidbody PRB = p.GetComponent<Rigidbody>();
+            PRB.AddForce(rb.velocity + PRB.transform.forward * ProjectileSpeed, ForceMode.VelocityChange);
+        }
+
+    }
+}
